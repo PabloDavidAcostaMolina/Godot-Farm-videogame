2bbe793828137c80f8d64f9d326df2e57848d97b
Pablo David Acosta Molina <roket644@gmail.com>
2023-03-28 10:51:10 -0500

    escena de jugador animada, walk, idle y transiciones

--- countrySide/Characters/PlayerCat.gd
+++ countrySide/Characters/PlayerCat.gd
@@ -1,21 +1,44 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export  var move_speed : float = 100
+export var starting_direction: Vector2 = Vector2.ZERO - Vector2(0, 1)
+
+onready var animation_tree: AnimationTree = $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
 
 var velocity = Vector2()
 
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed('right'):
-		velocity.x += 1
-	if Input.is_action_pressed('left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('down'):
-		velocity.y += 1
-	if Input.is_action_pressed('up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+func _ready():
+	update_animation_parameters(starting_direction)
 
-func _physics_process(delta):
-	get_input()
+
+func _physics_process(_delta):
+	#Establee la dirección de entrada
+	var input_direction = Vector2(
+	Input.get_action_strength("right") - Input.get_action_strength("left"),
+	Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	update_animation_parameters(input_direction)
+	
+	#Actualiza la velocidad
+	velocity = input_direction * move_speed
+	
+	#usa la velocidad del cuerpo del personaje para mover el personaje en el mapa
 	move_and_slide(velocity)
+	
+	#
+	pick_new_state()
+	
+func update_animation_parameters(move_input : Vector2):
+	#no cambia parametros de animación si no hay direccion de entrada
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/walk/blend_position", move_input)
+		animation_tree.set("parameters/idle/blend_position", move_input)
+		
+func pick_new_state():
+	# escoje el estado basandose en que está pasando con el jugador
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("walk")
+	else:
+		state_machine.travel("idle")
