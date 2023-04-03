398e1d739ace7b51c51d4a1ffeee7a8fbd55f514
Pablo David Acosta Molina <roket644@gmail.com>
2023-04-03 18:04:23 -0500

    agregado background, algunos tilesets, player con camara

--- countrySide/Characters/PlayerCat.gd
+++ countrySide/Characters/PlayerCat.gd
@@ -13,7 +13,7 @@ func _ready():
 
 
 func _physics_process(_delta):
-	#Establee la dirección de entrada
+	#Establece la dirección de entrada
 	var input_direction = Vector2(
 	Input.get_action_strength("right") - Input.get_action_strength("left"),
 	Input.get_action_strength("down") - Input.get_action_strength("up")
@@ -37,7 +37,7 @@ func update_animation_parameters(move_input : Vector2):
 		animation_tree.set("parameters/idle/blend_position", move_input)
 		
 func pick_new_state():
-	# escoje el estado basandose en que está pasando con el jugador
+	# escoje el estado basandose en qué está pasando con el jugador
 	if(velocity != Vector2.ZERO):
 		state_machine.travel("walk")
 	else:
